84e417832a0977561d8e190ca8a546bb41e7532f
Eduir Brazon <ebr0394@gmail.com>
2022-12-29 20:37:50 -0400

    events *a little practice of events using Js

---
+++ Events/scripts.js
@@ -0,0 +1,16 @@
+function Default() {
+  document.bgColor = "White";
+  document.fgColor = "Black";
+}
+function Red() {
+  document.bgColor = "Red";
+  document.fgColor = "Blue";
+}
+function Blue() {
+  document.bgColor = "Blue";
+  document.fgColor = "white";
+}
+function Yellow() {
+  document.bgColor = "yellow";
+  document.fgColor = "black";
+}
